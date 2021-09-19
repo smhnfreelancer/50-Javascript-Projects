f92583d543abd574b182f8a32ef9e346af75bc17
smhnfreelancer <77273247+smhnfreelancer@users.noreply.github.com>
2021-09-19 15:11:40 +0430

    New Lessons

--- project-19/scripts.js
+++ project-19/scripts.js
@@ -34,9 +34,10 @@ function setTime() {
     const hoursForClock = hours % 12;
     const minutes = time.getMinutes();
     const seconds = time.getSeconds();
-    let secondsScaled = (scale(seconds, 0, 59, 0, 360)) + secondsLap * 360;
-    let minutesScaled = (scale(minutes, 0, 59, 0, 360)) + minutesLap * 360;
-    let hoursScaled = (scale(hoursForClock, 0, 59, 0, 360)) + hoursLap * 360;
+    let secondsScaled = (scale(seconds, 0, 60, 0, 360)) + secondsLap * 360;
+    let minutesScaled = (scale(minutes, 0, 60, 0, 360)) + minutesLap * 360;
+    let hoursScaled = (scale(hoursForClock, 0, 60, 0, 360)) + hoursLap * 360;
+    let colon = `<span class="coloni">:</span>`;
 
     const date = time.getDate();
     const ampm = hours >= 12 ? 'PM' : 'AM';
@@ -49,17 +50,20 @@ function setTime() {
     if (hours == 59) {
         secondsLap++;
     }
+
     hourEl.style.transform = `translate(-50%, -100%) rotate(${hoursScaled}deg)`;
     minuteEl.style.transform = `translate(-50%, -100%) rotate(${minutesScaled}deg)`;
     secondEl.style.transform = `translate(-50%, -100%) rotate(${secondsScaled}deg)`;
     dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`;
-    timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm} `;
+    timeEl.innerHTML = `${hoursForClock == 0 ? `12` : `${hoursForClock}`}${colon}${minutes < 10 ? `0${minutes}` : minutes} ${ampm} `;
+
 
 }
 
 const scale = (number, inMin, inMax, outMin, outMax) => {
     return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
 };
+
 setTime();
 
 setInterval(setTime, 1000);
